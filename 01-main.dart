f3a387258a0dfb20aa68af9da5ae09caa32cda37
Tikiva <laura.e.granados@gmail.com>
2022-02-21 19:53:11 -0600

    Create 01-main.dart
    ¡Hola mundo!

---
+++ 01-main.dart
@@ -0,0 +1,14 @@
+void main() {
+   // Este es un comentario
+  
+   /* Este es otro comentario 
+      de varias líneas.
+      Asegurate de colocar el / 
+      inmediatamente posterior al *
+      para cerrar
+   */
+  
+  
+    print('¡Hola mundo!');
+  
+}
